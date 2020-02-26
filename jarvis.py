d3c6c58606a47ae8ac9abec7d777b1a405acc5a2
fredrickkemosi <fkemosi@gmail.com>
2020-02-26 22:56:52 +0300

    first commit

---
+++ jarvis.py
@@ -0,0 +1,130 @@
+from gtts import gTTS
+import speech_recognition as sr
+import re
+import time
+import webbrowser
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import smtplib
+import requests
+from pygame import mixer
+import urllib.request
+import urllib.parse
+import json
+import bs4
+
+
+def talk(audio):
+    "speaks audio passed as argument"
+
+    print(audio)
+    for line in audio.splitlines():
+        text_to_speech = gTTS(text=audio, lang="en-uk")
+        text_to_speech.save("audio.mp3")
+        mixer.init()
+        mixer.music.load("audio.mp3")
+        mixer.music.play()
+
+
+def myCommand():
+    "listens for commands"
+    # Initialize the recognizer
+    # The primary purpose of a Recognizer instance is, of course, to recognize speech.
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Jarvis is Ready...")
+        r.pause_threshold = 1
+        # wait for a second to let the recognizer adjust the
+        # energy threshold based on the surrounding noise level
+        r.adjust_for_ambient_noise(source, duration=1)
+        # listens for the user's input
+        audio = r.listen(source)
+        print("analyzing...")
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print("You said: " + command + "\n")
+        time.sleep(2)
+
+    # loop back to continue to listen for commands if unrecognizable speech is received
+    except sr.UnknownValueError:
+        print("Your last command couldn't be heard")
+        command = myCommand()
+
+    return command
+
+
+def jarvis(command):
+    errors = ["I don't know what you mean", "Excuse me?", "Can you repeat it please?"]
+    "if statements for executing commands"
+
+    # Search on Google
+    if "open google and search" in command:
+        reg_ex = re.search("open google and search (.*)", command)
+        search_for = command.split("search", 1)[1]
+        print(search_for)
+        url = "https://www.google.com/"
+        if reg_ex:
+            subgoogle = reg_ex.group(1)
+            url = url + "r/" + subgoogle
+        talk("Okay!")
+        driver = webdriver.Firefox(executable_path="/path/to/geckodriver")
+        driver.get("http://www.google.com")
+        search = driver.find_element_by_name("q")
+        search.send_keys(str(search_for))
+        search.send_keys(Keys.RETURN)  # hit return after you enter search text
+
+    # Search videos on Youtube and play (e.g. Search in youtube believer)
+    elif "youtube" in command:
+        talk("Ok!")
+        reg_ex = re.search("youtube (.+)", command)
+        if reg_ex:
+            domain = command.split("youtube", 1)[1]
+            query_string = urllib.parse.urlencode({"search_query": domain})
+            html_content = urllib.request.urlopen(
+                "http://www.youtube.com/results?" + query_string
+            )
+            search_results = re.findall(
+                r"href=\"\/watch\?v=(.{11})", html_content.read().decode()
+            )
+            # print("http://www.youtube.com/watch?v=" + search_results[0])
+            webbrowser.open(
+                "http://www.youtube.com/watch?v={}".format(search_results[0])
+            )
+            pass
+        
+    #  weather forecast in your city (e.g. weather in London)
+    # please create and use your own API it is free
+    elif "weather in" in command:
+        city = command.split("in", 1)[1]   
+        #openweathermap API
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=your_api_key&units=metric'.format(city)
+        response = requests.get(url)
+        data = response.json()
+        #print(data)
+        temp = data['main']['temp']
+        round_temp = int(round(temp))
+        talk('It is {} degree celcius in {}'.format(round_temp, city))
+        time.sleep(3)
+
+
+    elif "hello" in command:
+        talk("Hello! I am Jarvis. How can I help you?")
+        time.sleep(3)
+    elif "who are you" in command:
+        talk("I am one of four former U.S. Marine Corps tactical robots")
+        time.sleep(3)
+    else:
+        error = random.choice(errors)
+        talk(error)
+        time.sleep(3)
+
+
+talk("Jarvis is activated!")
+
+# loop to continue executing multiple commands
+while True:
+    time.sleep(4)
+    jarvis(myCommand())
